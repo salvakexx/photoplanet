3f55e45317a65e1e81e43d9476ee1932bfaa0279
salvakexx <kexxdon91@gmail.com>
2013-04-22 23:14:07 +0300

    ilovelinux

---
+++ photoplanet/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic import TemplateView
+
+def hello(request):
+	return render_to_response('hello.html',RequestContext(request))
+
+
+
+
+	
